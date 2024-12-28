8ed4024edc03bd63a9d4ce69efb87b5f99b42bf1
codeleo-cat <codeleocat@gmail.com>
2024-12-28 10:38:17 +0900

    add / jsx - isLogin, className

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,24 @@
+import './Main.css';
 const Main = () => {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+  const number = 10;
+
+  const user = {
+    name: 'sky',
+    isLogin: true,
+  };
+
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+
+  // return (
+  //   <main>
+  //     <h1>main</h1>
+  //     <h2>{number % 2 == 0 ? 'even' : 'odd'}</h2>
+  //   </main>
+  // );
 };
 
 export default Main;
