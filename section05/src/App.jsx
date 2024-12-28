4c389fbeb432c45bb05dc8c84c843e3c5187d459
codeleo-cat <codeleocat@gmail.com>
2024-12-28 15:16:47 +0900

    add / button event

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,17 +1,35 @@
 import './App.css';
-import Header from './components/Header'; // 확장자 생략 가능
-import Main from './components/Main';
-import Footer from './components/Footer';
+import { useState } from 'react';
 
 // 부모 component
 function App() {
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <h1>{light}</h1>
+      <button
+        // App component를 re-rendering
+        onClick={() => {
+          // setCount(count + 1);
+          setLight(light == 'ON' ? 'OFF' : 'ON');
+        }}
+      >
+        {light === 'ON' ? '끄기' : '켜기'}
+      </button>
     </>
   );
+
+  // return (
+  //   <>
+  //     <Button {...buttonProps} />
+  //     <Button text={'카페'} />
+  //     <Button text={'블로그'}>
+  //       <div>자식요소</div>
+  //     </Button>
+  //   </>
+  // );
 }
 
 export default App;
